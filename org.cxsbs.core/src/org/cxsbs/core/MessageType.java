b5537e156375324df6a583b3616f19241f378619
Morgan Borman <morgan.borman@gmail.com>
2012-08-12 10:28:21 -0700

    Added definitions of many more message types.

--- org.cxsbs.core/src/org/cxsbs/core/MessageType.java
+++ org.cxsbs.core/src/org/cxsbs/core/MessageType.java
@@ -26,98 +26,100 @@ public enum MessageType {
 	DIED(MessageContext.STC, 11, new Field[] { new IntField("clientnum"), new IntField("killer"), new IntField("frags") }),
 	DAMAGE(MessageContext.STC, 12, new Field[] { new IntField("clientnum"), new IntField("aggressor"), new IntField("damage"), new IntField("armour"), new IntField("health") }),
 	HITPUSH(MessageContext.STC, 13, new Field[] { new IntField("clientnum"), new IntField("gun"), new IntField("damage"), new VectorField("direction") }),
-	SHOTFX(MessageContext.STC, 14, new Field[] { }),
-	EXPLODEFX(MessageContext.STC, 15, new Field[] { }),
+	SHOTFX(MessageContext.STC, 14, new Field[] { new IntField("clientnum"), new IntField("gun"), new IntField("shotid"), new VectorField("from"), new VectorField("to")}),
+	EXPLODEFX(MessageContext.STC, 15, new Field[] { new IntField("clientnum"), new IntField("gun"), new IntField("shotid") }),
 	TRYSPAWN(MessageContext.CTS, 16, new Field[] { }),
-	SPAWNSTATE(MessageContext.STC, 17, new Field[] { }),
-	STC_SPAWN(MessageContext.STC, 18, new Field[] { }),
-	CTS_SPAWN(MessageContext.CTS, 18, new Field[] { }),
-	FORCEDEATH(MessageContext.STC, 19, new Field[] { }),
-	GUNSELECT(MessageContext.COM, 20, new Field[] { }),
+	SPAWNSTATE(MessageContext.STC, 17, new Field[] { }),//TODO: Need a AmmoListField field type
+	STC_SPAWN(MessageContext.STC, 18, new Field[] { }),//TODO: Need a ClientStateField field type
+	CTS_SPAWN(MessageContext.CTS, 18, new Field[] { new IntField("lifesequence"), new IntField("gunselect") }),
+	FORCEDEATH(MessageContext.STC, 19, new Field[] { new IntField("clientnum") }),
+	GUNSELECT(MessageContext.COM, 20, new Field[] { new IntField("gun") }),
 	TAUNT(MessageContext.COM, 21, new Field[] { }),
-	MAPCHANGE(MessageContext.COM, 22, new Field[] { }),
-	MAPVOTE(MessageContext.CTS, 23, new Field[] { }),
-	ITEMSPAWN(MessageContext.STC, 24, new Field[] { }),
-	ITEMPICKUP(MessageContext.CTS, 25, new Field[] { }),
-	ITEMACC(MessageContext.STC, 26, new Field[] { }),
-	TELEPORT(MessageContext.COM, 27, new Field[] { }),
-	JUMPPAD(MessageContext.COM, 28, new Field[] { }),
-	PING(MessageContext.CTS, 29, new Field[] { }),
-	PONG(MessageContext.STC, 30, new Field[] { }),
-	CLIENTPING(MessageContext.COM, 31, new Field[] { }),
-	TIMEUP(MessageContext.STC, 32, new Field[] { }),
+	CTS_MAPCHANGE(MessageContext.CTS, 22, new Field[] { new StringField("map"), new IntField("mode") }),
+	STC_MAPCHANGE(MessageContext.STC, 22, new Field[] { new StringField("map"), new IntField("mode"), new IntField("senditems") }),
+	MAPVOTE(MessageContext.CTS, 23, new Field[] { new StringField("map"), new IntField("mode") }),
+	ITEMSPAWN(MessageContext.STC, 24, new Field[] { new IntField("itemid") }),
+	ITEMPICKUP(MessageContext.CTS, 25, new Field[] { new IntField("itemid") }),
+	ITEMACC(MessageContext.STC, 26, new Field[] { new IntField("itemid"), new IntField("clientnum") }),
+	TELEPORT(MessageContext.COM, 27, new Field[] { new IntField("clientnum"), new IntField("teleport"), new IntField("teledest") }),
+	JUMPPAD(MessageContext.COM, 28, new Field[] { new IntField("clientnum"), new IntField("jumppad") }),
+	PING(MessageContext.CTS, 29, new Field[] { new IntField("clientmillis") }),
+	PONG(MessageContext.STC, 30, new Field[] { new IntField("clientmillis") }),
+	CLIENTPING(MessageContext.COM, 31, new Field[] { new IntField("ping") }),
+	TIMEUP(MessageContext.STC, 32, new Field[] { new IntField("timeleft") }),
 	MAPRELOAD(MessageContext.STC, 33, new Field[] { }),
 	FORCEINTERMISSION(MessageContext.CTS, 34, new Field[] { }),
-	SERVMSG(MessageContext.STC, 35, new Field[] { }),
-	ITEMLIST(MessageContext.COM, 36, new Field[] { }),
-	RESUME(MessageContext.STC, 37, new Field[] { }),
-	EDITMODE(MessageContext.COM, 38, new Field[] { }),
-	EDITENT(MessageContext.COM, 39, new Field[] { }),
-	EDITF(MessageContext.COM, 40, new Field[] { }),
-	EDITT(MessageContext.COM, 41, new Field[] { }),
-	EDITM(MessageContext.COM, 42, new Field[] { }),
-	FLIP(MessageContext.COM, 43, new Field[] { }),
-	COPY(MessageContext.COM, 44, new Field[] { }),
-	PASTE(MessageContext.COM, 45, new Field[] { }),
-	ROTATE(MessageContext.COM, 46, new Field[] { }),
-	REPLACE(MessageContext.COM, 47, new Field[] { }),
-	DELCUBE(MessageContext.COM, 48, new Field[] { }),
-	REMIP(MessageContext.COM, 49, new Field[] { }),
-	NEWMAP(MessageContext.COM, 50, new Field[] { }),
-	GETMAP(MessageContext.CTS, 51, new Field[] { }),
-	SENDMAP(MessageContext.STC, 52, new Field[] { }),
-	CLIPBOARD(MessageContext.COM, 53, new Field[] { }),
-	EDITVAR(MessageContext.COM, 54, new Field[] { }),
-	MASTERMODE(MessageContext.COM, 55, new Field[] { }),
-	KICK(MessageContext.CTS, 56, new Field[] { }),
+	SERVMSG(MessageContext.STC, 35, new Field[] { new StringField("text") }),
+	ITEMLIST(MessageContext.COM, 36, new Field[] { }),//TODO 
+	RESUME(MessageContext.STC, 37, new Field[] { }),//TODO
+	EDITMODE(MessageContext.COM, 38, new Field[] { new IntField("editting") }),
+	EDITENT(MessageContext.COM, 39, new Field[] { new IntField("entid"), new VectorField("position"), new IntField("type"), new IntField("attr1"), new IntField("attr2"), new IntField("attr3"), new IntField("attr4"), new IntField("attr5") }),
+	EDITF(MessageContext.COM, 40, new Field[] { }),//TODO editing messages
+	EDITT(MessageContext.COM, 41, new Field[] { }),//TODO editing messages
+	EDITM(MessageContext.COM, 42, new Field[] { }),//TODO editing messages
+	FLIP(MessageContext.COM, 43, new Field[] { }),//TODO editing messages
+	COPY(MessageContext.COM, 44, new Field[] { }),//TODO editing messages
+	PASTE(MessageContext.COM, 45, new Field[] { }),//TODO editing messages
+	ROTATE(MessageContext.COM, 46, new Field[] { }),//TODO editing messages
+	REPLACE(MessageContext.COM, 47, new Field[] { }),//TODO editing messages
+	DELCUBE(MessageContext.COM, 48, new Field[] { }),//TODO editing messages
+	REMIP(MessageContext.COM, 49, new Field[] { }),//TODO editing messages
+	NEWMAP(MessageContext.COM, 50, new Field[] { }),//TODO editing messages
+	GETMAP(MessageContext.CTS, 51, new Field[] { }),//TODO editing messages
+	SENDMAP(MessageContext.STC, 52, new Field[] { }),//TODO editing messages
+	CLIPBOARD(MessageContext.COM, 53, new Field[] { }),//TODO editing messages
+	EDITVAR(MessageContext.COM, 54, new Field[] { }),//TODO editing messages
+	MASTERMODE(MessageContext.COM, 55, new Field[] { new IntField("mastermode") }),
+	KICK(MessageContext.CTS, 56, new Field[] { new IntField("clientnum") }),
 	CLEARBANS(MessageContext.CTS, 57, new Field[] { }),
-	CURRENTMASTER(MessageContext.STC, 58, new Field[] { }),
-	SPECTATOR(MessageContext.COM, 59, new Field[] { }),
-	SETMASTER(MessageContext.CTS, 60, new Field[] { }),
-	SETTEAM(MessageContext.COM, 61, new Field[] { }),
-	CTS_BASES(MessageContext.CTS, 62, new Field[] { }),
-	STC_BASES(MessageContext.STC, 62, new Field[] { }),
-	BASEINFO(MessageContext.STC, 63, new Field[] { }),
-	BASESCORE(MessageContext.STC, 64, new Field[] { }),
-	REPAMMO(MessageContext.STC, 65, new Field[] { }),
-	BASEREGEN(MessageContext.STC, 66, new Field[] { }),
-	ANNOUNCE(MessageContext.STC, 67, new Field[] { }),
+	CURRENTMASTER(MessageContext.STC, 58, new Field[] { new IntField("clientnum"), new IntField("privilege"), new IntField("mastermode") }),
+	SPECTATOR(MessageContext.COM, 59, new Field[] { new IntField("clientnum"), new IntField("spectated") }),
+	SETMASTER(MessageContext.CTS, 60, new Field[] { new IntField("tryclaim"), new StringField("pwdhash") }),
+	CTS_SETTEAM(MessageContext.CTS, 61, new Field[] { new IntField("clientnum"), new StringField("team") }),
+	STC_SETTEAM(MessageContext.STC, 61, new Field[] { new IntField("clientnum"), new StringField("team"), new IntField("reason") }),
+	CTS_BASES(MessageContext.CTS, 62, new Field[] { }),//TODO capture modes
+	STC_BASES(MessageContext.STC, 62, new Field[] { }),//TODO capture modes
+	BASEINFO(MessageContext.STC, 63, new Field[] { }),//TODO capture modes
+	BASESCORE(MessageContext.STC, 64, new Field[] { }),//TODO capture modes
+	REPAMMO(MessageContext.STC, 65, new Field[] { }),//TODO capture modes
+	BASEREGEN(MessageContext.STC, 66, new Field[] { }),//TODO capture modes
+	ANNOUNCE(MessageContext.STC, 67, new Field[] { new IntField("type") }),
 	LISTDEMOS(MessageContext.CTS, 68, new Field[] { }),
-	SENDDEMOLIST(MessageContext.STC, 69, new Field[] { }),
-	GETDEMO(MessageContext.CTS, 70, new Field[] { }),
-	SENDDEMO(MessageContext.STC, 71, new Field[] { }),
-	DEMOPLAYBACK(MessageContext.STC, 72, new Field[] { }),
-	RECORDDEMO(MessageContext.CTS, 73, new Field[] { }),
+	SENDDEMOLIST(MessageContext.STC, 69, new Field[] {  }),//TODO iteration for demo list retrieval
+	GETDEMO(MessageContext.CTS, 70, new Field[] { new IntField("demonum") }),
+	SENDDEMO(MessageContext.STC, 71, new Field[] { }),//TODO incoming files
+	DEMOPLAYBACK(MessageContext.STC, 72, new Field[] { new IntField("value"), new IntField("clientnum") }),
+	RECORDDEMO(MessageContext.CTS, 73, new Field[] { new IntField("record") }),
 	STOPDEMO(MessageContext.CTS, 74, new Field[] { }),
-	CLEARDEMOS(MessageContext.CTS, 75, new Field[] { }),
-	CTS_TAKEFLAG(MessageContext.CTS, 76, new Field[] { }),
-	STC_TAKEFLAG(MessageContext.STC, 76, new Field[] { }),
-	RETURNFLAG(MessageContext.STC, 77, new Field[] { }),
-	RESETFLAG(MessageContext.STC, 78, new Field[] { }),
-	INVISFLAG(MessageContext.STC, 79, new Field[] { }),
-	TRYDROPFLAG(MessageContext.CTS, 80, new Field[] { }),
-	DROPFLAG(MessageContext.STC, 81, new Field[] { }),
-	SCOREFLAG(MessageContext.STC, 82, new Field[] { }),
-	CTS_INITFLAGS(MessageContext.CTS, 83, new Field[] { }),
-	STC_INITFLAGS(MessageContext.STC, 83, new Field[] { }),
-	SAYTEAM(MessageContext.COM, 84, new Field[] { }),
-	CLIENT(MessageContext.STC, 85, new Field[] { }),
-	AUTHTRY(MessageContext.CTS, 86, new Field[] { }),
-	AUTHCHAL(MessageContext.STC, 87, new Field[] { }),
-	AUTHANS(MessageContext.CTS, 88, new Field[] { }),
-	REQAUTH(MessageContext.STC, 89, new Field[] { }),
-	PAUSEGAME(MessageContext.COM, 90, new Field[] { }),
-	ADDBOT(MessageContext.CTS, 91, new Field[] { }),
-	DELBOT(MessageContext.CTS, 92, new Field[] { }),
-	INITAI(MessageContext.STC, 93, new Field[] { }),
-	FROMAI(MessageContext.CTS, 94, new Field[] { }),
-	BOTLIMIT(MessageContext.CTS, 95, new Field[] { }),
-	BOTBALANCE(MessageContext.CTS, 96, new Field[] { }),
-	MAPCRC(MessageContext.CTS, 97, new Field[] { }),
+	CLEARDEMOS(MessageContext.CTS, 75, new Field[] { new IntField("demonum") }),
+	CTS_TAKEFLAG(MessageContext.CTS, 76, new Field[] { }),//TODO flag modes
+	STC_TAKEFLAG(MessageContext.STC, 76, new Field[] { }),//TODO flag modes
+	RETURNFLAG(MessageContext.STC, 77, new Field[] { }),//TODO flag modes
+	RESETFLAG(MessageContext.STC, 78, new Field[] { }),//TODO flag modes
+	INVISFLAG(MessageContext.STC, 79, new Field[] { }),//TODO flag modes
+	TRYDROPFLAG(MessageContext.CTS, 80, new Field[] { }),//TODO flag modes
+	DROPFLAG(MessageContext.STC, 81, new Field[] { }),//TODO flag modes
+	SCOREFLAG(MessageContext.STC, 82, new Field[] { }),//TODO flag modes
+	CTS_INITFLAGS(MessageContext.CTS, 83, new Field[] { }),//TODO flag modes
+	STC_INITFLAGS(MessageContext.STC, 83, new Field[] { }),//TODO flag modes
+	SAYTEAM(MessageContext.COM, 84, new Field[] { new IntField("text") }),
+	CLIENT(MessageContext.STC, 85, new Field[] { }),//TODO
+	AUTHTRY(MessageContext.CTS, 86, new Field[] { new StringField("domain"), new StringField("authname") }),
+	AUTHCHAL(MessageContext.STC, 87, new Field[] { new StringField("domain"), new UintField("authid"), new StringField("challenge") }),
+	AUTHANS(MessageContext.CTS, 88, new Field[] { new StringField("domain"), new UintField("authid"), new StringField("answerhash") }),
+	REQAUTH(MessageContext.STC, 89, new Field[] { new StringField("domain") }),
+	PAUSEGAME(MessageContext.COM, 90, new Field[] { new IntField("value") }),
+	ADDBOT(MessageContext.CTS, 91, new Field[] { }),//TODO bots
+	DELBOT(MessageContext.CTS, 92, new Field[] { }),//TODO bots
+	INITAI(MessageContext.STC, 93, new Field[] { }),//TODO bots
+	FROMAI(MessageContext.CTS, 94, new Field[] { }),//TODO bots
+	BOTLIMIT(MessageContext.CTS, 95, new Field[] { }),//TODO bots
+	BOTBALANCE(MessageContext.CTS, 96, new Field[] { }),//TODO bots
+	MAPCRC(MessageContext.CTS, 97, new Field[] { new StringField("map"), new IntField("crc") }),
 	CHECKMAPS(MessageContext.CTS, 98, new Field[] { }),
-	SWITCHNAME(MessageContext.COM, 99, new Field[] { }),
-	SWITCHMODEL(MessageContext.COM, 100, new Field[] { }),
-	SWITCHTEAM(MessageContext.CTS, 101, new Field[] { }),
+	SWITCHNAME(MessageContext.COM, 99, new Field[] { new StringField("name") }),
+	SWITCHMODEL(MessageContext.COM, 100, new Field[] { new IntField("playermodel") }),
+	SWITCHTEAM(MessageContext.CTS, 101, new Field[] { new StringField("team") }),
 	NUMSV(MessageContext.NONE, 102, new Field[] { });
 	
 	private final MessageContext context;
